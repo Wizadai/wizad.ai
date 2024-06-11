0d38a936e566a806e55512f0a518ce86146a86b8
Re-Dei <hridaya.pradhan123@gmail.com>
2024-06-11 21:41:06 +0545

    fix: blog header

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,9 +3,6 @@ import Footer from "@/app/_elements/Footer";
 import Header from "@/app/_elements/Header";
 import BlogTile from "@/app/blogs/_elements/BlogTile";
 import { getBlogPosts } from "@/app/blogs/utils";
-import Image from "next/image";
-import Link from "next/link";
-import { TfiArrowTopRight } from "react-icons/tfi";
 
 export default async function Page() {
   const blogs = await getBlogPosts();
@@ -15,13 +12,15 @@ export default async function Page() {
       <Header />
 
       <section className="flex w-full flex-col bg-hero-gradient bg-cover bg-top py-14 md:py-24">
-        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-6 px-4 md:items-start md:gap-10">
-          <h1 className="max-w-3xl font-hero text-4xl font-semibold italic md:text-8xl">
-            Articles to help you get started
+        <div className="mx-auto flex w-full max-w-6xl flex-col items-center gap-6 px-4 md:items-start md:gap-10">
+          <h1 className="max-w-3xl font-hero text-4xl font-semibold italic md:text-7xl">
+            <div>{"Articles to help "}</div>
+            <div>you get started</div>
           </h1>
-          <span className="max-w-3xl text-base font-medium text-white/70 md:text-2xl">
-            Must-read articles and trends on social media, branding, marketing,
-            and ever changing AI creative industry.
+          <span className="max-w-3xl text-base text-center md:text-left font-medium text-white/70 md:text-xl">
+            Must-read articles and trends on social media, branding,
+            <br className="hidden md:block" />
+            marketing, and ever changing AI creative industry.
           </span>
         </div>
       </section>
