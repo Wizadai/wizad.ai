bbbed63b072189c11cc3509a22bb416bbbc01881
Re-Dei <hridaya.pradhan123@gmail.com>
2024-07-03 14:03:00 +0545

    fix: production url

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,12 +1,5 @@
 export const getBaseUrl = () => {
-  const env = process.env.VERCEL_ENV;
-  let baseUrl = "http://localhost:3000";
-
-  if (env === "production") {
-    baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL!;
-  } else if (env === "preview" || env === "development") {
-    baseUrl = process.env.VERCEL_URL!;
-  }
+  let baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL || "http://localhost:3000";
 
   return baseUrl;
 };
