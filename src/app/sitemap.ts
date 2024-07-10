91d536bff0867e000b26e602c8f382f18febf318
Re-Dei <hridaya.pradhan123@gmail.com>
2024-07-10 18:49:47 +0545

    refactor: freq and priority in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,11 +6,15 @@ export default async function sitemap() {
   const blogs = (await getBlogPosts()).map((post) => ({
     url: `${baseUrl}/blogs/${post.slug}`,
     lastModified: post.metadata.publishedAt,
+    frequency: "never",
+    priority: 0.7,
   }));
 
   const routes = ["", "/blogs"].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
+    frequency: "never",
+    priority: 1,
   }));
 
   return [...routes, ...blogs];
