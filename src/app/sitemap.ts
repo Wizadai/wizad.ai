b6220200cb38d69e08d1604a85745bee0b80cf43
Re-Dei <hridaya.pradhan123@gmail.com>
2024-07-10 19:09:33 +0545

    fix: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,28 +1,40 @@
 import { getBlogPosts } from "@/app/blogs/utils";
+import { MetadataRoute } from "next";
 
 export const baseUrl = process.env.NEXT_PUBLIC_URL!;
 
-export default async function sitemap() {
-  const blogs = (await getBlogPosts()).map((post) => ({
-    url: `${baseUrl}/blogs/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-    frequency: "never",
-    priority: 0.7,
-  }));
+type Site = ArrayElement<MetadataRoute.Sitemap>;
 
-  const staticRoutes = [""].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split("T")[0],
-    frequency: "never",
-    priority: 1,
-  }));
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const blogs: MetadataRoute.Sitemap = (await getBlogPosts()).map((post) => {
+    const site: Site = {
+      url: `${baseUrl}/blogs/${post.slug}`,
+      lastModified: post.metadata.publishedAt,
+      changeFrequency: "never",
+      priority: 0.7,
+    };
+    return site;
+  });
 
-  const dynamicRoutes = ["/blogs"].map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split("T")[0],
-    frequency: "weekly",
-    priority: 1,
-  }));
+  const staticRoutes: MetadataRoute.Sitemap = [""].map((route) => {
+    const site: Site = {
+      url: `${baseUrl}${route}`,
+      lastModified: new Date().toISOString().split("T")[0],
+      changeFrequency: "never",
+      priority: 1,
+    };
+    return site;
+  });
+
+  const dynamicRoutes: MetadataRoute.Sitemap = ["/blogs"].map((route) => {
+    const site: Site = {
+      url: `${baseUrl}${route}`,
+      lastModified: new Date().toISOString().split("T")[0],
+      changeFrequency: "weekly",
+      priority: 1,
+    };
+    return site;
+  });
 
   return [...staticRoutes, ...dynamicRoutes, ...blogs];
 }
